ebcf344713ce814f03cf2212a02ec51831149f48
Marinellys Figueroa <marinellys.figueroa@gmail.com>
2020-08-10 21:05:43 -0500

    truncar text

---
+++ src/app/pipe/truncar-text.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TruncarTextPipe } from './truncar-text.pipe';
+
+describe('TruncarTextPipe', () => {
+  it('create an instance', () => {
+    const pipe = new TruncarTextPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
